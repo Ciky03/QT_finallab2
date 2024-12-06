c0ddad9ca6bce8703509ad9ddd09caa7d33de856
Ciky03 <Ciky03@users.noreply.github.com>
2024-12-06 23:44:40 +0800

    初始化基本的系统托盘

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMenu>
+#include <QStyle>
+#include <QApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , trayIcon(new QSystemTrayIcon(this))
 {
     ui->setupUi(this);
+    
+    // 设置窗口标题
+    setWindowTitle(tr("日历日程"));
+    
+    // 初始化系统托盘
+    trayIcon->setIcon(style()->standardIcon(QStyle::SP_ComputerIcon));
+    trayIcon->setToolTip(tr("日历日程"));
+    
+    // 创建托盘菜单
+    QMenu *trayMenu = new QMenu(this);
+    trayMenu->addAction(tr("显示主窗口"), this, &QWidget::show);
+    trayMenu->addAction(tr("退出"), qApp, &QApplication::quit);
+    
+    trayIcon->setContextMenu(trayMenu);
+    trayIcon->show();
 }
 
 MainWindow::~MainWindow()
